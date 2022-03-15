5415f19ce9fec0729cbd3f323f098d6d966d1e6e
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-03-15 01:27:47 +0000

    adding questions and choices and correct answers

---
+++ resources/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "At the first meeting of the Duelling Club, Draco Malfoy summoned what animal with the spell ‘Serpensortia’?",
+        choices: ["Frog", "Bear", "Snake", "Dragon"],
+        answer: "Snake"
+    },
+    {
+        title: "The 'Felifors' spell turns a cat into what?",
+        choices: ["Matchbox", "Cauldron", "Hat", "Bat"],
+        answer: "Cauldron"
+    },
+    {
+        title: "Which element is associated with Hufflepuff?",
+        choices: ["Earth", "Fire", "Water", "Air"],
+        answer: "Earth"
+    },
+    {
+        title: "What was the name of the Black family’s house elf?",
+        choices: ["Bobby", "Winky", "Dobby", "Kreacher"],
+        answer: "Dobby"
+    },
+    {
+        title: "What position does Harry play on his Quidditch team?",
+        choices: ["Chaser", "Seeker", "Bludger", "Keeper"],
+        answer: "Seeker"
+    }
+]
